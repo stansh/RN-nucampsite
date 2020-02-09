3a59137278134dff9fac844fcb71f7a06489e4f2
Stan S <stan.shm@gmail.com>
2020-02-08 19:58:53 -0600

    Forms

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -12,6 +12,7 @@ import SafeAreaView from 'react-native-safe-area-view';
 import { connect } from 'react-redux';
 import { fetchCampsites, fetchComments, fetchPromotions,
     fetchPartners } from '../redux/ActionCreators';
+import Reservation from './ReservationComponent';
 
 const mapDispatchToProps = {
     fetchCampsites,
@@ -146,6 +147,29 @@ const CustomDrawerContentComponent = props => (
     </ScrollView>
 );
 
+// reservation navigator
+const ReservationNavigator = createStackNavigator(
+    {
+        Reservation: { screen: Reservation }
+    },
+    {
+        navigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='tree'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
 
 // main navigator
 
@@ -191,6 +215,22 @@ const MainNavigator = createDrawerNavigator(
                 )
             }
         },
+
+        Reservation: {
+            screen: ReservationNavigator,
+            navigationOptions: {
+                drawerLabel: 'Reserve Campsite',
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='tree'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
+        
         Contact: {
             screen: ContactNavigator,
             navigationOptions: {
@@ -204,7 +244,10 @@ const MainNavigator = createDrawerNavigator(
                     />
                 )
             }
-        }
+        },
+
+        
+
     },
     {
         drawerBackgroundColor: '#CEC8FF',
