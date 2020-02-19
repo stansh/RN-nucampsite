cba31471a23e2b74f211277478d393401c610544
Stan S <stan.shm@gmail.com>
2020-02-18 20:34:27 -0600

    Swipeout and Delete Favorites

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -4,6 +4,8 @@ import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import Swipeout from 'react-native-swipeout';
+import { deleteFavorite } from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
@@ -12,6 +14,12 @@ const mapStateToProps = state => {
     };
 };
 
+const mapDispatchToProps = {    
+    deleteFavorite: campsiteId => (deleteFavorite(campsiteId))};
+
+
+
+    
 class Favorites extends Component {
 
     static navigationOptions = {
@@ -21,15 +29,26 @@ class Favorites extends Component {
     render() {
         const { navigate } = this.props.navigation;
         const renderFavoriteItem = ({item}) => {
+            const rightButton = [
+                {
+                    text: 'Delete',
+                    type: 'delete',
+                    onPress: () => this.props.deleteFavorite(item.id)
+                }
+            ];
+        
             return (
-                <ListItem
-                    title={item.name}
-                    subtitle={item.description}
-                    leftAvatar={{source: {uri: baseUrl + item.image}}}
-                    onPress={() => navigate('CampsiteInfo', {campsiteId: item.id})}
-                />
+                <Swipeout right={rightButton} autoClose={true}>
+                    <ListItem
+                        title={item.name}
+                        subtitle={item.description}
+                        leftAvatar={{source: {uri: baseUrl + item.image}}}
+                        onPress={() => navigate('CampsiteInfo', {campsiteId: item.id})}
+                    />
+                </Swipeout>
             );
         };
+        
 
         if (this.props.campsites.isLoading) {
             return <Loading />;
@@ -53,4 +72,4 @@ class Favorites extends Component {
     }
 }
 
-export default connect(mapStateToProps)(Favorites);
+export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
